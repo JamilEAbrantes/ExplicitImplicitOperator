7dcc67566639cef780d4fb38e39a1e1a5742e28d
jamil.abrantes <jamilabrantes.dev@outlook.com>
2019-12-03 17:00:27 -0300

    Add project files.

---
+++ ExplicitImplicitOperator.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExplicitImplicitOperator.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
